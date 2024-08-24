616babbd64395528606d8ad1c04d6d7007c53d9c
SHIBIN K A <shibin.k.a6235@gmail.com>
2024-08-24 22:17:48 +0530

    refactor : .env file data added and changed

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -2,14 +2,17 @@ import { DataSource } from "typeorm";
 import { Airport } from "../models/airport.entity";
 import { Country } from "../models/country.entity";
 import { City } from "../models/city.entity";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
+    type: process.env.DB_TYPE as 'postgres',
+    host: process.env.DB_HOST,
     port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "hava_havai",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: false,
     entities: [Airport, Country, City],
